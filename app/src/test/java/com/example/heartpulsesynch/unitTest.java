e845c765c0c7ff5b5c914a224851f2e9d27d940f
Ryan <exp@chamber1.ryn>
2023-07-06 09:25:44 +0600

    uloading javadoc

--- app/src/test/java/com/example/heartpulsesynch/unitTest.java
+++ app/src/test/java/com/example/heartpulsesynch/unitTest.java
@@ -7,47 +7,53 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+/**
+ * A class containing unit tests for the {@link RecordList} class.
+ */
 public class unitTest {
 
+    /**
+     * Tests the {@link RecordList#addRecord(ModelClass)} method.
+     */
     @Test
-    public void testAddRecord(){
-        RecordList recordList= new RecordList();
-        ModelClass modelClass= new ModelClass("1-1-2000","10:20","89","67","67","dummy");
+    public void testAddRecord() {
+        RecordList recordList = new RecordList();
+        ModelClass modelClass = new ModelClass("1-1-2000", "10:20", "89", "67", "67", "dummy");
         recordList.addRecord(modelClass);
         assertEquals(1, recordList.mcl.size());
 
-        ModelClass modelClass1=new ModelClass("1-1-2000","10:20","89","67","67","dummy");
+        ModelClass modelClass1 = new ModelClass("1-1-2000", "10:20", "89", "67", "67", "dummy");
         recordList.addRecord(modelClass1);
-        assertEquals(2,recordList.mcl.size());
+        assertEquals(2, recordList.mcl.size());
         assertTrue(recordList.mcl.contains(modelClass));
         assertTrue(recordList.mcl.contains(modelClass1));
-
     }
+
     /**
-     *
+     * Tests the {@link RecordList#addRecord(ModelClass)} method when adding a duplicate record.
      */
     @Test
-    public void addRecordExTest(){
-        RecordList recordList= new RecordList();
-        ModelClass modelClass= new ModelClass("1-1-2000","10:20","89","67","67","dummy");
+    public void addRecordExTest() {
+        RecordList recordList = new RecordList();
+        ModelClass modelClass = new ModelClass("1-1-2000", "10:20", "89", "67", "67", "dummy");
         recordList.addRecord(modelClass);
         assertThrows(IllegalArgumentException.class, () -> recordList.addRecord(modelClass));
     }
+
+    /**
+     * Tests the {@link RecordList#deleteRecord(int)} method.
+     */
     @Test
     public void testDeleteRecord() {
-        //int position=0;
         RecordList recordList = new RecordList();
         ModelClass record1 = new ModelClass("1-1-2000", "10:20", "89", "67", "67", "dummy");
         recordList.addRecord(record1);
         assertEquals(1, recordList.mcl.size());
 
-        RecordList recordList1 = new RecordList();
         ModelClass record2 = new ModelClass("1-1-2000", "10:20", "89", "67", "67", "dummy");
         recordList.addRecord(record2);
         assertEquals(2, recordList.mcl.size());
 
-
-        RecordList recordList2 = new RecordList();
         ModelClass record3 = new ModelClass("1-1-2000", "10:20", "89", "67", "67", "dummy");
         recordList.addRecord(record3);
         assertEquals(3, recordList.mcl.size());
@@ -65,8 +71,12 @@ public class unitTest {
 
         assertThrows(IllegalArgumentException.class, () -> recordList.deleteRecord(1));
     }
+
+    /**
+     * Tests the {@link RecordList#count()} method.
+     */
     @Test
-public void testCount(){
+    public void testCount(){
         RecordList recordList=new RecordList();
         ModelClass modelClass1=new ModelClass("1-1-2000","10:20","89","67","67","dummy");
         recordList.addRecord(modelClass1);
